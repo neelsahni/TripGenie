f6ea0727df55e73e07a56c9c636ac84789b716c3
Ludwigvsch <lvschoenfeldt@gmx.de>
2023-04-22 17:12:46 -0700

    init Airbnb files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:ai_trip_planner/screens/WelcomeScreen.dart';
+import 'package:ai_trip_planner/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,56 +16,25 @@ class MyApp extends StatelessWidget {
   final _navigatorKey = GlobalKey<NavigatorState>();
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => ThemeNotifier(),
-      
-      child: Consumer<ThemeNotifier>(
-        builder: (context, ThemeNotifier notifier, child) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError || snapshot == null) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData) {
+            return MaterialApp(
 
-        return FutureBuilder(
-            future: Firebase.initializeApp(),
-            builder: (context, snapshot) {
-              // Check for errors
-              if (snapshot.hasError || snapshot == null) {
-                return Center(child: CircularProgressIndicator());
-              } else if (snapshot.connectionState == ConnectionState.done &&
-                  snapshot.hasData) {
-                return MaterialApp(
-                    localizationsDelegates: [
-                      GlobalMaterialLocalizations.delegate,
-                      GlobalWidgetsLocalizations.delegate,
-                      GlobalCupertinoLocalizations.delegate,
-                    ],
-                    supportedLocales: [
-                      const Locale('en', 'US'),
-                      const Locale('es', 'ES'),
-                      const Locale('de', 'DE'),
-                    ],
-                    theme: notifier.darkMode ? dark_mode : light_mode,
-                    navigatorKey: _navigatorKey,
-                    debugShowCheckedModeBanner: false,
-                    title: 'Welcome to the Growth Planner',
-                    //home: WelcomeScreenWidget(),
-                    builder: (context, child) {
-                      return MediaQuery(
-                        child: child!,
-                        data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-                      );
-                    },
-                    //TODO: start using everywhere...
-                    initialRoute: 'Welcome',
-                    
-                 
-                );
-              } else {
-                return Center(child: CircularProgressIndicator());
-              }
-            });
-
-      },
-
-
-      ),
-    );
+                navigatorKey: _navigatorKey,
+                debugShowCheckedModeBanner: false,
+                title: 'AI TRIP PLANNER',
+                home: const MyHomePage(title: 'Airbnb Flutter Clone Project'),
+             
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        });
   }
 }
